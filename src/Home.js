5df7651958448dd9c0243d659e0a5724a5e27765
June Idea <juneidea@yahoo.com>
2019-03-14 23:34:09 -0500

    move pdf

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
+import resume from './assets/JuneSuparoekResume.pdf';
 
 const Home = () => {
     return(
@@ -9,7 +10,7 @@ const Home = () => {
                 <h1>Hi, my name is <span className="highlight">June Suparoek</span>. I build things for people.</h1>
                 <p>I'm a full-stack software developer who enjoys working at the
                 intersection of engineering and design. I'm currently looking for a job as a <span className="highlight">front end developer</span></p>
-                <a href="/public/JuneSuparoekResume.pdf" target="_blank" className="button-link">Resume</a>
+                <a href={resume} target="_blank" className="button-link">Resume</a>
                 </div>
             </section>
             <Footer />
