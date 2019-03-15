eee9515ddb113bd8420adbedccef286f69665de2
June Idea <juneidea@yahoo.com>
2019-03-14 23:42:04 -0500

    pdf target

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <h1>Hi, my name is <span className="highlight">June Suparoek</span>. I build things for people.</h1>
                 <p>I'm a full-stack software developer who enjoys working at the
                 intersection of engineering and design. I'm currently looking for a job as a <span className="highlight">front end developer</span></p>
-                <a href={resume} target="_blank" className="button-link">Resume</a>
+                <a href={resume} target="_blank" rel="noopener noreferrer" className="button-link">Resume</a>
                 </div>
             </section>
             <Footer />
